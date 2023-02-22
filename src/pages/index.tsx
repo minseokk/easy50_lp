bc0dc770dd01f9a71523c1dd3d2903ad2b6dc78a
minseokk <minseokk@postech.ac.kr>
2023-02-22 17:39:10 +0900

    page 1 styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ const Home: NextPage = () => {
             통해 쉽고 편하게 일하세요.
           </p>
         </div>
-        <div className="flex w-full justify-center">
+        <div className="mb-6 flex w-full justify-center">
           <Link href={"/application"}>
             <button
               type="button"
@@ -49,12 +49,14 @@ const Home: NextPage = () => {
             </button>
           </Link>
         </div>
-        <div className="w-full">
+        <div className="mb-12 w-full">
           <h2 className="text-3xl">기능 소개</h2>
           <table className="m-auto w-full table-auto border-collapse">
             <tbody>
               <tr>
-                <td className="border">안전보건관리체계 업무</td>
+                <td className="border border-white bg-yellow-400 text-center text-xl text-white">
+                  안전보건관리체계 업무
+                </td>
                 <td className="border">
                   <ul>
                     <li>안전보건경영 방침 수립</li>
@@ -65,7 +67,9 @@ const Home: NextPage = () => {
                 </td>
               </tr>
               <tr>
-                <td className="border">위험성평가</td>
+                <td className="border border-white bg-yellow-400 text-center text-xl text-white">
+                  위험성평가
+                </td>
                 <td className="border">
                   <ul>
                     <li>위험성 평가 수행 및 기록</li>
@@ -73,7 +77,9 @@ const Home: NextPage = () => {
                 </td>
               </tr>
               <tr>
-                <td className="border">종사자 의견청취</td>
+                <td className="border border-white bg-yellow-400 text-center text-xl text-white">
+                  종사자 의견청취
+                </td>
                 <td className="border">
                   <ul>
                     <li>안전에 대한 종사자 설문 설계와 취합, 서류화, 보관</li>
@@ -81,7 +87,9 @@ const Home: NextPage = () => {
                 </td>
               </tr>
               <tr>
-                <td className="border">안전 교육</td>
+                <td className="border border-white bg-yellow-400 text-center text-xl text-white">
+                  안전 교육
+                </td>
                 <td className="border">
                   <ul>
                     <li>안전교육 계획, 수행 및 캠페인 추진에 대한 기록</li>
@@ -89,7 +97,9 @@ const Home: NextPage = () => {
                 </td>
               </tr>
               <tr>
-                <td className="border">산업재해 대응</td>
+                <td className="border border-white bg-yellow-400 text-center text-xl text-white">
+                  산업재해 대응
+                </td>
                 <td className="border">
                   <ul>
                     <li>내부 통계 기록을 활용한 원인조사</li>
@@ -99,7 +109,9 @@ const Home: NextPage = () => {
                 </td>
               </tr>
               <tr>
-                <td className="border">산업재해 통계</td>
+                <td className="border border-white bg-yellow-400 text-center text-xl text-white">
+                  산업재해 통계
+                </td>
                 <td className="border">
                   <ul>
                     <li>KOSHA 산업재해 통계 자료 확인 링크</li>
@@ -107,7 +119,9 @@ const Home: NextPage = () => {
                 </td>
               </tr>
               <tr>
-                <td className="border">법령안내</td>
+                <td className="border border-white bg-yellow-400 text-center text-xl text-white">
+                  법령안내
+                </td>
                 <td className="border">
                   <ul>
                     <li>산업안전보건법</li>
@@ -117,7 +131,9 @@ const Home: NextPage = () => {
                 </td>
               </tr>
               <tr>
-                <td className="border">안전회의 및 TBM</td>
+                <td className="border border-white bg-yellow-400 text-center text-xl text-white">
+                  안전회의 및 TBM
+                </td>
                 <td className="border">
                   <ul>
                     <li>회의 안건 공유</li>
@@ -127,7 +143,9 @@ const Home: NextPage = () => {
                 </td>
               </tr>
               <tr>
-                <td className="border">데이터 저장소</td>
+                <td className="border border-white bg-yellow-400 text-center text-xl text-white">
+                  데이터 저장소
+                </td>
                 <td className="border">
                   <ul>
                     <li>모든 데이터 열람</li>
@@ -138,7 +156,7 @@ const Home: NextPage = () => {
             </tbody>
           </table>
         </div>
-        <div>
+        <div className="mb-12 w-full">
           <h2 className="text-3xl">데이터 보관 안내</h2>
           <p>
             모든 안전보건 활동 데이터는 3년 이상 편안50 서버에 안전하게
@@ -146,7 +164,7 @@ const Home: NextPage = () => {
             안전보건체계 구축 증빙자료를 제출할 수 있습니다.
           </p>
         </div>
-        <div>
+        <div className="mb-12 w-full">
           <h2 className="text-3xl">요금제</h2>
           <p>월 10만 원~30만 원(변동가능) 데이터 보관료: 3년 초과 시 별도</p>
         </div>
